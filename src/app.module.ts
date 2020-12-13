7ee7286a5cca621fb568fba239e8477eadb216fa
Egor Polupan <egor.polupan@factorialcomplexity.com>
2020-12-13 21:05:19 +0200

    Add mysql and typeorm to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { TodoListsModule } from './todo-lists/todo-lists.module';
+import { TodoListsModule } from './todo-lists/todo-lists.module'
 
 @Module({
-  imports: [TodoListsModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'mysql',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: true,
+        synchronize: true // only for develop
+      })
+    }),
+    ConfigModule.forRoot(),
+    TodoListsModule
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
