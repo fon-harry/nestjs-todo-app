3527dc1c791d6c99e660f0bf4196abc3120d77d7
Egor Polupan <egor.polupan@factorialcomplexity.com>
2020-12-13 21:19:05 +0200

    Add todo list repository; update create/findOne methods to work with repository

--- src/todo-lists/todo-lists.service.ts
+++ src/todo-lists/todo-lists.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
 import { CreateTodoListDto } from './dto/create-todo-list.dto'
+import { TodoList } from './entities/todo-list.entity'
 
 @Injectable()
 export class TodoListsService {
+  constructor(
+    @InjectRepository(TodoList)
+    private readonly todoListRepository: Repository<TodoList>
+  ) {}
+
   create(createTodoListDto: CreateTodoListDto) {
-    return `This action adds a new todoList: ${createTodoListDto.name}`
+    const todoList = this.todoListRepository.create(createTodoListDto)
+    return this.todoListRepository.save(todoList)
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} todoList`
+    return this.todoListRepository.findOne(id)
   }
 }
