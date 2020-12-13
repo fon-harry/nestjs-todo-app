9e812e7a105467c02ba33e6a065b67274cf01dd6
Egor Polupan <egor.polupan@factorialcomplexity.com>
2020-12-13 22:19:12 +0200

    Add swagger to project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
+
+const packageJson = require('../package.json')
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const options = new DocumentBuilder()
+    .setTitle('TODO')
+    .setDescription('Application API')
+    .setVersion(packageJson.version)
+    .build()
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('api/docs', app, document)
+
   await app.listen(3000)
 }
 bootstrap()
