e87822a33ecdf44e38980d4d064340582daaa315
Egor Polupan <egor.polupan@factorialcomplexity.com>
2020-12-14 15:42:01 +0200

    Add routes summary description to swagger documentation

--- src/todo-lists/todo-lists.controller.ts
+++ src/todo-lists/todo-lists.controller.ts
@@ -1,18 +1,20 @@
 import { Controller, Get, Post, Body, Param, ParseIntPipe } from '@nestjs/common'
 import { TodoListsService } from './todo-lists.service'
 import { CreateTodoListDto } from './dto/create-todo-list.dto'
-import { ApiTags } from '@nestjs/swagger'
+import { ApiOperation, ApiTags } from '@nestjs/swagger'
 
 @ApiTags('Todo lists')
 @Controller('api/todo-lists')
 export class TodoListsController {
   constructor(private readonly todoListsService: TodoListsService) {}
 
+  @ApiOperation({ summary: 'Create new todo list' })
   @Post()
   create(@Body() createTodoListDto: CreateTodoListDto) {
     return this.todoListsService.create(createTodoListDto)
   }
 
+  @ApiOperation({ summary: 'Get todo list' })
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.todoListsService.findOne(id)
--- src/todo-lists/todos/todos.controller.ts
+++ src/todo-lists/todos/todos.controller.ts
@@ -2,23 +2,26 @@ import { Controller, Post, Body, Param, Delete, ParseIntPipe, Patch } from '@nes
 import { TodosService } from './todos.service'
 import { CreateTodoDto } from './dto/create-todo.dto'
 import { UpdateTodoDto } from './dto/update-todo.dto'
-import { ApiTags } from '@nestjs/swagger'
+import { ApiOperation, ApiTags } from '@nestjs/swagger'
 
 @ApiTags('Todos')
 @Controller('api/todos')
 export class TodosController {
   constructor(private readonly todosService: TodosService) {}
 
+  @ApiOperation({ summary: 'Create new todo' })
   @Post()
   create(@Body() createTodoDto: CreateTodoDto) {
     return this.todosService.create(createTodoDto)
   }
 
+  @ApiOperation({ summary: 'Update todo' })
   @Patch(':id')
   update(@Param('id', ParseIntPipe) id: number, @Body() updateTodoDto: UpdateTodoDto) {
     return this.todosService.update(id, updateTodoDto)
   }
 
+  @ApiOperation({ summary: 'Delete todo' })
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.todosService.remove(id)
